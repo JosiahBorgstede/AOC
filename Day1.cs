1753dc2a2fe0a7006b49235ee7d910f3f3fa2532
Josiah Borgstede <borgstede.j@northeastern.edu>
2024-12-02 12:00:54 -0500

    messing around with making day 1 run faster

--- Day1.cs
+++ Day1.cs
@@ -1,9 +1,10 @@
 
+using System.Diagnostics;
 using System.Text.RegularExpressions;
 
 public class Day1 {
     public static void Part1(string path) {
-        List<string> lines = File.ReadAllLines(path).ToList();
+        IEnumerable<string> lines = File.ReadLines(path);
         Regex getNumbers = new Regex(@"(?<num1>\d*)   (?<num2>\d*)");
         IEnumerable<int> first = lines.Select(line => {
             Match val = getNumbers.Match(line);
@@ -16,8 +17,8 @@ public class Day1 {
         Console.WriteLine(first.Zip(second).Select(pair => int.Abs(pair.First - pair.Second)).Sum());
     }
 
-    public static void Part2(string path) {
-        List<string> lines = File.ReadAllLines(path).ToList();
+    public static void Part2(IEnumerable<string> lines) {
+        //IEnumerable<string> lines = File.ReadLines(path);
         Regex getNumbers = new Regex(@"(?<num1>\d*)   (?<num2>\d*)");
         IEnumerable<int> first = lines.Select(line => {
             Match val = getNumbers.Match(line);
@@ -28,5 +29,43 @@ public class Day1 {
             return int.Parse(val.Groups["num2"].ToString());
         }).Order();
         Console.WriteLine(first.Select(inVal => inVal * second.Count(twoVal => twoVal == inVal)).Sum());
+    }
+
+    public static void part1Testing(string path) {
+        IEnumerable<string> lines = File.ReadLines(path);
+        List<int> first = new List<int>();
+        List<int> second = new List<int>();
+        foreach(var line in lines) {
+            IEnumerable<int> numbers = line.Split("   ").Select(int.Parse);
+            first.Add(numbers.First());
+            second.Add(numbers.Last());
+        }
+        first.Sort();
+        second.Sort();
+        Console.WriteLine(first.Zip(second).Select(pair => int.Abs(pair.First - pair.Second)).Sum());
+    }
+
+    public static void part2Testing(IEnumerable<string> lines) {
+        //IEnumerable<string> lines = File.ReadLines(path);
+        List<int> first = new List<int>();
+        Dictionary<int, int> second = new();
+        foreach(var line in lines) {
+            IEnumerable<int> numbers = line.Split("   ").Select(int.Parse);
+            first.Add(numbers.First());
+            second[numbers.Last()] = second.TryGetValue(numbers.Last(), out int val) ? val + 1 : 1;
+        }
+        Console.WriteLine(first.Select(inVal => inVal * (second.TryGetValue(inVal, out int val) ? val : 0)).Sum());
+    }
+
+    public static void Part2WithTimings(string path) {
+        var lines = File.ReadLines(path).ToList();
+        var watch = Stopwatch.StartNew();
+        part2Testing(lines);
+        watch.Stop();
+        Console.WriteLine(watch.ElapsedMilliseconds);
+        watch.Restart();
+        Part2(lines);
+        watch.Stop();
+        Console.WriteLine(watch.ElapsedMilliseconds);
     }
 }
