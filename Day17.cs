3e2715b1decbd6ca9b8dfaffe3f62bcbee26ba8c
Josiah Borgstede <borgstede.j@northeastern.edu>
2024-12-18 07:18:21 -0500

    attempts to make a better solution for 17

--- Day17.cs
+++ Day17.cs
@@ -24,7 +24,7 @@ public class Day17 : IDay
             regs[i] = int.Parse(vals[1]);
         }
         Registers registers = new(regs[0], regs[1], regs[2]);
-        List<long> program = lines[4].Split(": ")[1].Split(",").Select(long.Parse).ToList();
+        List<int> program = lines[4].Split(": ")[1].Split(",").Select(int.Parse).ToList();
         List<long> outputs = RunProgram(program, registers);
         StringBuilder res = new();
         foreach(var output in outputs) {
@@ -34,12 +34,12 @@ public class Day17 : IDay
         return res.ToString().TrimEnd(',');
     }
 
-    public List<long> RunProgram(List<long> program, Registers registers, bool JmpEneabled = true) {
+    public List<long> RunProgram(List<int> program, Registers registers) {
         List<long> outputs = [];
         Registers curRegs = registers;
         for(int i = 0; i < program.Count; i +=2) {
             long opcode = program[i];
-            long operand = program[i+1];
+            int operand = program[i+1];
             switch (opcode) {
                 case 0:
                     int denom = (int) Math.Pow(2, GetComboOperand(operand, curRegs));
@@ -55,8 +55,8 @@ public class Day17 : IDay
                     curRegs = curRegs with {B = val % 8};
                     break;
                 case 3:
-                    if(curRegs.A != 0 && JmpEneabled) {
-                        i = (int)operand - 2;
+                    if(curRegs.A != 0) {
+                        i = operand - 2;
                     }
                     break;
                 case 4:
@@ -94,7 +94,7 @@ public class Day17 : IDay
         _ => throw new Exception("unknown operand"),
     };
 
-    public string Part2(string path)
+    public string Part2Old(string path)
     {
         List<string> lines = File.ReadLines(path).ToList();
         int[] regs = new int[3];
@@ -103,7 +103,7 @@ public class Day17 : IDay
             regs[i] = int.Parse(vals[1]);
         }
         Registers registers = new(regs[0], regs[1], regs[2]);
-        List<long> program = lines[4].Split(": ")[1].Split(",").Select(long.Parse).ToList();
+        List<int> program = lines[4].Split(": ")[1].Split(",").Select(int.Parse).ToList();
         registers = registers with {A = (long)Math.Pow(8, 15)};
         List<long> outputs = RunProgram(program, registers);
         writeOutput(outputs);
@@ -119,7 +119,7 @@ public class Day17 : IDay
                 writeOutput(outputs);
             }
         }
-        while(!outputs.SequenceEqual(program)) {
+        while(!outputs.SequenceEqual(program.ConvertAll<long>(x => x))) {
             long curA = registers.A;
             registers = registers with {A = curA + 1};
             Console.WriteLine("value of A:" + registers.A);
@@ -130,6 +130,98 @@ public class Day17 : IDay
         return registers.A.ToString();
     }
 
+    public string ExpandProg(List<int> program, Registers registers) {
+        StringBuilder outputs = new();
+        Registers curRegs = registers;
+        for(int i = 0; i < program.Count; i += 2) {
+            long opcode = program[i];
+            long operand = program[i+1];
+            switch (opcode) {
+                case 0:
+
+                    int denom = (int) Math.Pow(2, GetComboOperand(operand, curRegs));
+                    long num = curRegs.A;
+                    curRegs = curRegs with {A = num/denom};
+                    outputs.Append($"A = A({num})/{GetComboOperatorRep(operand)}^2({denom}): {curRegs.A}\n");
+                    break;
+                case 1:
+                    long curB = curRegs.B;
+                    curRegs = curRegs with {B = curB ^ operand};
+                    outputs.Append($"B = B({curB}) XOR {operand}: {curRegs.B}\n");
+                    break;
+                case 2:
+                    long val = GetComboOperand(operand, curRegs);
+                    curRegs = curRegs with {B = val % 8};
+                    outputs.Append($"B = {GetComboOperatorRep(operand)}({val}) % 8: {curRegs.B}\n");
+
+                    break;
+                case 3:
+                    if(curRegs.A != 0) {
+                        i = (int)operand - 2;
+                        outputs.Append($"Jumping to {operand}\n");
+                    }
+                    break;
+                case 4:
+                    long currentB = curRegs.B;
+                    long currentC = curRegs.C;
+                    curRegs = curRegs with {B = currentB ^ currentC};
+                    outputs.Append($"B = B({currentB}) XOR C({currentC}): {curRegs.B}\n");
+                    break;
+                case 5:
+                    long curVal = GetComboOperand(operand, curRegs);
+                    outputs.Append($"output: {GetComboOperatorRep(operand)}({curVal}) % 8: {curVal % 8}\n");
+                    break;
+                case 6:
+                    denom = (int) Math.Pow(2, GetComboOperand(operand, curRegs));
+                    num = curRegs.A;
+                    curRegs = curRegs with {B = num/denom};
+                    outputs.Append($"B = A({num})/{GetComboOperatorRep(operand)}^2({denom}): {curRegs.B}\n");
+                    break;
+                case 7:
+                    denom = (int) Math.Pow(2, GetComboOperand(operand, curRegs));
+                    num = curRegs.A;
+                    curRegs = curRegs with {C = num/denom};
+                    outputs.Append($"C = A({num})/{GetComboOperatorRep(operand)}^2({denom}): {curRegs.C}\n");
+                    break;
+            }
+        }
+        return outputs.ToString();
+    }
+
+    public static char GetComboOperatorRep(long operand) => operand switch
+    {
+        1 => '1',
+        2 => '2',
+        3 => '3',
+        4 => 'A',
+        5 => 'B',
+        6 => 'C',
+        _ => throw new Exception("unknown operand"),
+    };
+
+    public string Part2(string path)
+    {
+        List<string> lines = File.ReadLines(path).ToList();
+        int[] regs = new int[3];
+        for(int i = 0; i < 3; i++) {
+            string[] vals = lines[i].Split(": ");
+            regs[i] = int.Parse(vals[1]);
+        }
+        Registers registers = new(regs[0], regs[1], regs[2]);
+        List<int> program = lines[4].Split(": ")[1].Split(",").Select(int.Parse).ToList();
+        string expanded = ExpandProg(program, registers);
+        Console.Write(expanded);
+        for(int i = 0; i < 64; i++) {
+            long res = FastCalcA(i);
+            Console.Write(res + " ");
+        }
+        return registers.A.ToString();
+    }
+
+    public static long FastCalcA(long A) 
+    {
+        return (((A ^ 1) ^ 5) ^ (A / (long)Math.Pow(2,A ^ 1))) % 8;
+    }
     public void writeOutput(List<long> outputs) {
         StringBuilder res = new();
         foreach(var output in outputs) {
